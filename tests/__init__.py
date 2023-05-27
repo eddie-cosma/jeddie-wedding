12b4864f2da4c6bef5ec1fc6feefab273d3a1800
Eddie Cosma <eddie.cosma@gmail.com>
2023-05-27 17:06:03 -0400

    Built out automated tests for RSVP flow

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,33 +6,58 @@ from jeddie.database.model import Guest, Party, Item, Gift
 
 
 def load_database_test_data(session):
-    party_1 = Party(name='Jane Doe and John Roe')
+    party_1 = Party(
+        name='Jane Doe and John Roe',
+        uuid='97a0b846-a798-41a2-80de-cf0d81a1f596'
+    )
     guest_1 = Guest(
+        id=1,
         first_name='John',
         last_name='Doe',
-        party=party_1
+        party=party_1,
+        attending=True
     )
     guest_2 = Guest(
+        id=2,
         first_name='Jane',
         last_name='Roe',
-        party=party_1
+        party=party_1,
+        attending=False
     )
-    party_2 = Party(name='Ben and Becky Smith')
+    party_2 = Party(
+        name='Ben and Becky Smith',
+        uuid='40820dfd-fd9d-4be7-872f-c57df7506328'
+    )
     guest_3 = Guest(
+        id=3,
         first_name='Ben',
         last_name='Smith',
         party=party_2
     )
     guest_4 = Guest(
+        id=4,
         first_name='Becky',
         last_name='Smith',
         party=party_2
     )
-    party_3 = Party(name='James Doe')
+    party_3 = Party(
+        name='James Doe',
+        uuid='e8ea4d4d-c6ae-4b23-95a3-e91cc511521f'
+    )
     guest_5 = Guest(
+        id=5,
         first_name='James',
         last_name='Doe',
-        party=party_3
+        party=party_3,
+        attending=True
+    )
+    guest_6 = Guest(
+        id=6,
+        first_name='Guest of James',
+        last_name='Doe',
+        is_plus_one=True,
+        party=party_3,
+        attending=True
     )
     item_1 = Item(
         name='Test Item 1',
@@ -57,7 +82,7 @@ def load_database_test_data(session):
         item=item_2,
         quantity=1
     )
-    for item in [party_1, guest_1, guest_2, party_2, guest_3, guest_4, party_3, guest_5,
+    for item in [party_1, guest_1, guest_2, party_2, guest_3, guest_4, party_3, guest_5, guest_6,
                  item_1, item_2, gift_1]:
         session.add(item)
     session.commit()
--- tests/test_rsvp.py
+++ tests/test_rsvp.py
@@ -1,5 +1,6 @@
 import pytest
 
+from database.model import Party
 from . import app, client, runner
 
 
@@ -53,3 +54,115 @@ def test_rsvp_multiple_results_same_party_post(client, name):
     response = client.post('/en/rsvp_search', data=payload, follow_redirects=True)
     assert '<h2>Ben Smith</h2>' in response.text
     assert '<h2>Becky Smith</h2>' in response.text
+
+
+def test_rsvp_invalid_party(client):
+    response = client.get('/en/rsvp_detail/123456')
+    assert response.status_code == 302
+
+    response = client.get('/en/rsvp_detail/123456', follow_redirects=True)
+    assert '<h1>RSVP Search</h1>' in response.text
+
+
+def test_rsvp_empty_rsvp_response(client):
+    response = client.post('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328', data={})
+    assert response.status_code == 302
+
+    response = client.post('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328', data={}, follow_redirects=True)
+    assert 'Thank you for your reservation.' in response.text
+
+
+def test_rsvp_nobody_coming_response(client):
+    payload = {
+        '3': '0',
+        '4': '0'
+    }
+    response = client.post('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328', data=payload)
+    assert response.status_code == 302
+
+    response = client.post('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328', data=payload, follow_redirects=True)
+    assert 'Thank you for your reservation.' in response.text
+
+
+def test_rsvp_someone_coming_response(client):
+    payload = {
+        '3': '1',
+        '4': '0'
+    }
+    response = client.post('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328', data=payload)
+    assert response.status_code == 302
+
+    response = client.post('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328', data=payload, follow_redirects=True)
+    assert '<h1>Ben Smith</h1>' in response.text
+    assert '<h2>Meal choice</h2>' in response.text
+
+
+def test_rsvp_invalid_party_on_guest_detail(client):
+    response = client.get('/en/rsvp_detail/123456/1')
+    assert response.status_code == 302
+
+    response = client.get('/en/rsvp_detail/123456/1', follow_redirects=True)
+    assert '<h1>RSVP Search</h1>' in response.text
+
+
+def test_rsvp_invalid_guest_for_party(client):
+    response = client.get('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328/0')
+    assert response.status_code == 302
+
+    response = client.get('/en/rsvp_detail/40820dfd-fd9d-4be7-872f-c57df7506328/0', follow_redirects=True)
+    assert '<h1>RSVP Search</h1>' in response.text
+
+
+def test_rsvp_missing_name_response(client):
+    payload = {
+        'meal': '2',
+        'dietary_restrictions': '',
+        'song_choice': ''
+    }
+    response = client.post('/en/rsvp_detail/e8ea4d4d-c6ae-4b23-95a3-e91cc511521f/6', data=payload, follow_redirects=True)
+    assert response.status_code == 200
+    assert '<h1>Guest of James Doe</h1>' in response.text
+    assert 'Please enter your first and last name.' in response.text
+
+
+def test_rsvp_missing_meal_response(client):
+    payload = {
+        'first_name': 'First',
+        'last_name': 'Last',
+        'meal': '',
+        'dietary_restrictions': '',
+        'song_choice': ''
+    }
+    response = client.post('/en/rsvp_detail/e8ea4d4d-c6ae-4b23-95a3-e91cc511521f/6', data=payload)
+    assert response.status_code == 200
+    assert '<h1>First Last</h1>' in response.text
+    assert 'Please select a meal option.' in response.text
+
+
+def test_rsvp_successful_rsvp_with_more_guests(client):
+    payload = {
+        'meal': '1',
+        'dietary_restrictions': '',
+        'song_choice': ''
+    }
+    response = client.post('/en/rsvp_detail/e8ea4d4d-c6ae-4b23-95a3-e91cc511521f/5', data=payload)
+    assert response.status_code == 302
+
+    response = client.post('/en/rsvp_detail/e8ea4d4d-c6ae-4b23-95a3-e91cc511521f/5', data=payload, follow_redirects=True)
+    assert '<h1>Guest of James Doe</h1>' in response.text
+
+
+def test_rsvp_successful_rsvp_with_no_more_guests(client):
+    payload = {
+        'first_name': 'First',
+        'last_name': 'Last',
+        'meal': '1',
+        'dietary_restrictions': '',
+        'song_choice': ''
+    }
+    response = client.post('/en/rsvp_detail/97a0b846-a798-41a2-80de-cf0d81a1f596/1', data=payload)
+    assert response.status_code == 302
+
+    response = client.post('/en/rsvp_detail/97a0b846-a798-41a2-80de-cf0d81a1f596/1', data=payload, follow_redirects=True)
+    assert '<h1>RSVP Search</h1>' in response.text
+
