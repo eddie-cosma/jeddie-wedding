973a1b78198db00a82f5c6dc377c086c2091ea28
Eddie Cosma <eddie.cosma@gmail.com>
2023-05-28 12:53:50 -0400

    Added additional email headers directly to message to improve compliance. Added threading to email handling so it will not block clients from moving forward.

--- jeddie/middleware/email.py
+++ jeddie/middleware/email.py
@@ -1,13 +1,27 @@
 import smtplib
+from threading import Thread
 
 from config import config
-from database.model import Item
 
 
-def log_to_email(item: Item, buyer: str, stripe_id: str):
-    smtp = smtplib.SMTP(config.EMAIL_HOSTNAME, 587)
-    smtp.starttls()
-    smtp.login(config.EMAIL_USERNAME, config.EMAIL_PASSWORD)
-    message = f'Gift of ${item.price / 100} received from {buyer} via Stripe (transaction ID: {stripe_id}).'
-    smtp.sendmail(config.EMAIL_USERNAME, config.EMAIL_RECIPIENT, message)
-    smtp.quit()
+def threading(f):
+    def wrapper(*args, **kwargs):
+        thr = Thread(target=f, args=args, kwargs=kwargs)
+        thr.start()
+    return wrapper
+
+
+@threading
+def log_to_email(message: str):
+    if config.EMAIL_HOSTNAME and config.EMAIL_USERNAME \
+            and config.EMAIL_PASSWORD and config.EMAIL_RECIPIENT:
+        smtp = smtplib.SMTP(config.EMAIL_HOSTNAME, 587)
+        smtp.starttls()
+        headers = (
+            f'From: {config.EMAIL_USERNAME}\n'
+            f'To: {config.EMAIL_RECIPIENT}\n'
+            'Subject: jeddie.wedding notice\n\n'
+        )
+        smtp.login(config.EMAIL_USERNAME, config.EMAIL_PASSWORD)
+        smtp.sendmail(config.EMAIL_USERNAME, config.EMAIL_RECIPIENT, headers + message)
+        smtp.quit()
